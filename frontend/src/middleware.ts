ba8491195afcf4f04cb317d9b4a799e885dc4992
Yuri <yuri.regi7254@gmail.com>
2026-06-16 16:21:18 -0300

    fix: middleware signIn path

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   pages: {
-    signIn: "/admin/login", // Ensina ao middleware onde é a porta de entrada
+    signIn: "/login", // Ensina ao middleware onde é a porta de entrada
   },
 });
 
